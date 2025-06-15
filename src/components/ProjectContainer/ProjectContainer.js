eaba02830ad97e3d0cc8a4ee0c4646c54f628c5c
Alhadiy Shofi <alhadinindo@gmail.com>
2025-06-15 20:43:26 +0700

    update

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -1,43 +1,62 @@
 import uniqid from 'uniqid'
 import GitHubIcon from '@material-ui/icons/GitHub'
 import LaunchIcon from '@material-ui/icons/Launch'
+import { Link } from 'react-router-dom'
 import './ProjectContainer.css'
 
-const ProjectContainer = ({ project }) => (
-  <div className='project'>
-    <h3>{project.name}</h3>
+const ProjectContainer = ({ project }) => {
+  const isInternalLink = (url) => url.startsWith('/') || url.startsWith('#')
 
-    <p className='project__description'>{project.description}</p>
-    {project.stack && (
-      <ul className='project__stack'>
-        {project.stack.map((item) => (
-          <li key={uniqid()} className='project__stack-item'>
-            {item}
-          </li>
-        ))}
-      </ul>
-    )}
+  return (
+    <div className='project'>
+      <h3>{project.name}</h3>
 
-    {project.sourceCode && (
-      <a
-        href={project.sourceCode}
-        aria-label='source code'
-        className='link link--icon'
-      >
-        <GitHubIcon />
-      </a>
-    )}
+      <p className='project__description'>{project.description}</p>
+      {project.stack && (
+        <ul className='project__stack'>
+          {project.stack.map((item) => (
+            <li key={uniqid()} className='project__stack-item'>
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
 
-    {project.livePreview && (
-      <a
-        href={project.livePreview}
-        aria-label='live preview'
-        className='link link--icon'
-      >
-        <LaunchIcon />
-      </a>
-    )}
-  </div>
-)
+      {project.sourceCode && (
+        <a
+          href={project.sourceCode}
+          aria-label='source code'
+          className='link link--icon'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <GitHubIcon />
+        </a>
+      )}
+
+      {project.livePreview && (
+        isInternalLink(project.livePreview) ? (
+          <Link
+            to={project.livePreview}
+            aria-label='live preview'
+            className='link link--icon'
+          >
+            <LaunchIcon />
+          </Link>
+        ) : (
+          <a
+            href={project.livePreview}
+            aria-label='live preview'
+            className='link link--icon'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <LaunchIcon />
+          </a>
+        )
+      )}
+    </div>
+  )
+}
 
 export default ProjectContainer
